63ab47704cb5d08fcc3c1c6f1ae3b412686c1e02
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-06-26 18:18:27 +0300

    v.2.0

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,5 +1,6 @@
 import os
 
+
 the_board = [i for i in range(1,10)]
 the_board_2 = [i for i in range(1,10)]
 menu_answers = {'1':'statistic','2':'playgame'}
@@ -7,24 +8,35 @@ is_the_user_x = True
 is_the_user_0 = False
 player_number_x_or_0 = {True: 'X', False: '0'}
 move = False
+statistic_game = {'X':0, '0':0}
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
-
-def generator_player():
-    while True:
-        for i in range(0,2):
-            yield int(i)
 
 def change_player(move):
     change_move = not move
     return change_move
 
 def get_answer(wrong = 0):
-    if wrong > 1:
+    if wrong != 0:
         print('Pls pick the right option:')
     answer = input('Your choice: ')
     return menu_answers.get(answer)
+
+def show_menu():
+    print('__MENU__')
+    print('1. SHOW STATISTIC')
+    print('2. PLAY THE GAME\n')
+
+def player_choice():
+    show_menu()
+    answer = get_answer()
+    while not answer:
+        wrong = 1
+        cls()
+        show_menu()
+        answer = get_answer(wrong)
+    return answer
 
 def win_check():
     if (the_board[0] == the_board[1] == the_board[2]) or (the_board[3] == the_board[4] == the_board[5]) or (the_board[6] == the_board[7] == the_board[8]) \
@@ -37,8 +49,6 @@ or (the_board[0] == the_board[4] == the_board[8]) or (the_board[2] == the_board[
         result = 'next'
         
     return result
-    
-    
 
 def show_the_board():
     cls()
@@ -49,18 +59,6 @@ def show_the_board():
     print(f'-- | --- | --')
     print(f'{the_board[6]}  |  {the_board[7]}  |  {the_board[8]}\n')
 
-def show_menu(wrong = 0):
-    print('__MENU__')
-    print('1. SHOW STATISTIC')
-    print('2. PLAY THE GAME\n')
-    answer = get_answer(wrong)
-    while not answer:
-        wrong = 2
-        cls()
-        show_menu(wrong)
-        answer = get_answer()
-    return answer
-
 def get_user_turn():
     while True:
         try:
@@ -68,15 +66,16 @@ def get_user_turn():
             if result == 'next':
                 global move
                 move = change_player(move)
-                
+
                 turn = input(f'User {player_number_x_or_0.get(move)} turn (1-9):')
-                if int(turn) in the_board_2:
-                    print('OK')
-                    the_board_2.remove(int(turn))
-                    the_board[int(turn)-1] = player_number_x_or_0[move]
-                    return turn
-                else:
-                    print('Enter correct value')
+                while not int(turn) in the_board_2:
+                    print('Enter a free value')
+                    turn = input(f'User {player_number_x_or_0.get(move)} turn (1-9):')
+
+                print('OK')
+                the_board_2.remove(int(turn))
+                the_board[int(turn)-1] = player_number_x_or_0[move]
+                return turn
 
             elif result == 'draw':
                 print('End the game\n')
@@ -87,14 +86,19 @@ def get_user_turn():
             elif result == 'finish':
                 print('End the game\n')
                 print(f'Winner is Player {player_number_x_or_0.get(move)}!')
+                statistic_game[player_number_x_or_0.get(move)] += 1
                 turn = 'finish'
                 return turn
         except:
-            print('Enter correct value') 
+            print('Enter correct value')
+            move = change_player(move) 
 
-                  
+  
 def show_statistic():
-    print('Showing statistic')
+    print('Showing statistic:')
+    print(statistic_game)
+    input('Lets continue? :)\nPush any buttom\n')
+    main()
 
 def play_the_game():
     show_the_board()
@@ -109,11 +113,9 @@ def play_the_game():
         else:
             show_the_board()
         
-
-
 def main():
     cls()
-    answer = show_menu()
+    answer = player_choice()
     if answer == 'statistic':
         show_statistic()
     else: 
@@ -121,7 +123,6 @@ def main():
         input('One more time? :)\nPush any buttom\n')
         main()
     pass
-
 
 if __name__ == '__main__':
     main()
